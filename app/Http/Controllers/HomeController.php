f9afcbc4a0b44808685487d400844205de3f8f8e
khaleddoosama <khaleddoosama@gmail.com>
2024-12-24 03:42:07 +0200

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
 
     public function index()
     {
-        dd(request()->header('x-forwarded-proto'));
         Log::info('X-Forwarded-Proto1: ' . request()->header('x-forwarded-proto'));
 
         return view('auth.selection');
